8c24db519a35bda43c9d5f0543aad04b19c73fcd
Lokeshsinh <162404534+Lokeshsinh@users.noreply.github.com>
2025-12-17 10:51:02 +0530

    Implement anagram check with user input

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
+const inputValue1  =  document.querySelector("#input1");
+const inputValue2 =  document.querySelector("#input2");
+const btn  = document.querySelector("#btn");
+const msg  =  document.querySelector("#msg")
 
+btn.addEventListener("click", () => {
+    const input1  = inputValue1.value
+    const input2  =  inputValue2.value;
+
+    const word1  =  input1.split("").sort().join("");
+    const word2 =  input2.split("").sort().join("");
+
+
+    if(word1 === "" && word2 === ""){
+        alert("Enter inputs letters")
+        return
+    }
+
+    if(word1 === word2){
+        msg.innerHTML = "Anagram"
         msg.style.color  = "green"
     }else{
         msg.innerHTML = "Not Anagram"
         msg.style.color = "red"
     }
+})
 
 })
